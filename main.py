426faec3d036c79a46daa8a21ce0d165adab70bb
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-27 11:48:43 +0100

    refactor: add first shape of main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Authors : Lorenzo OTTAVIANI, Olivier PORTAL et Thibault CARON.
+Date : 27/01/2025 11h45
+Aim of the program :
+    Execute a typing fruit game with PyGame.
+Inputs :
+Output :
+"""
+
+
+def main():
+    """"""
+    pass
+
+
+if __name__ == "__main__":  # The program will be run only if executed directly, not if it is called by another program.
+    main()
