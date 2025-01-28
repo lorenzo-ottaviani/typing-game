a84c68d14ebbf972c0e2e381ad2fea9907970446
Thibault Caron <thibault.caron@laplateforme.io>
2025-01-28 11:30:26 +0100

    feat: test key inputs

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ Aim of the program :
 Inputs :
 Output :
 """
-
+import pygame
+import random
+screen = pygame.display.set_mode((640, 240))
 
 def main():
     """"""
-    pass
+    app_running = True
+    while app_running:
+        
+        for event in pygame.event.get():
+            print(event)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == 27 or event.key == 13: #esc or enter keys
+                    app_running = False
+
+        if event.type == pygame.QUIT:
+            app_running = False
+
+        # if event.unicode == "/x1b":
+        #     app_running = False
 
 
 if __name__ == "__main__":  # The program will be run only if executed directly, not if it is called by another program.
