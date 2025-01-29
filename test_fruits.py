c87371fa4636156cb7e3f9acf7883b3f00e6c153
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-29 17:09:25 +0100

    feat: add one more fruit to the game!

--- test_fruits.py
+++ test_fruits.py
@@ -4,6 +4,7 @@ from random import uniform
 from functions.CONFIGS import *
 
 apple = pygame.image.load("assets/images/fruits/apple.png")
+pear = pygame.image.load("assets/images/fruits/pear.png")
 # apple = pygame.transform.scale(apple, (90, 90))
 
 pygame.init()
@@ -14,12 +15,21 @@ clock = pygame.time.Clock()
 x_min = 6/14 * SCREEN_WIDTH
 x_max = 8/14 * SCREEN_WIDTH
 
-x = uniform(x_min, x_max)
+objet = {"type": "random(object_types)", "x": uniform(x_min, x_max), "y": SCREEN_HEIGHT, "speed_x": uniform(-7, 7),
+         "speed_y": uniform(-17, -15)}
 
-y = SCREEN_HEIGHT
+x_apple = uniform(x_min, x_max)
 
-speed_x = uniform(-7, 7)
-speed_y = uniform(-17, -15)
+x_pear = uniform(x_min, x_max)
+
+y_apple = SCREEN_HEIGHT
+y_pear = SCREEN_HEIGHT
+
+speed_x_apple = uniform(-7, 7)
+speed_y_apple = uniform(-17, -15)
+
+speed_x_pear = uniform(-7, 7)
+speed_y_pear = uniform(-17, -15)
 
 t0 = pygame.time.get_ticks()  # Initial time
 
@@ -33,11 +43,15 @@ while running:
 
     t = (pygame.time.get_ticks() - t0) / 400  # Count the time during movement.
 
-    x += speed_x
-    y += speed_y * t + 5 * t**2
+    x_apple += speed_x_apple
+    y_apple += speed_y_apple * t + 5 * t**2
+
+    x_pear += speed_x_pear
+    y_pear += speed_y_pear * t + 5 * t ** 2
 
     screen.fill((0, 0, 0))  # Cancel old place of image during movement.
-    screen.blit(apple, (x,y))
+    screen.blit(apple, (x_apple, y_apple))
+    screen.blit(pear, (x_pear, y_pear))
     pygame.display.flip()
 
     clock.tick(60)
