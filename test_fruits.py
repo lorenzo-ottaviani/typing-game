9e1a43185cc1a1e46e22358a31c0b0d0c74663df
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-01-30 11:02:48 +0100

    feat: add differents random choices for the two fruits

--- test_fruits.py
+++ test_fruits.py
@@ -15,23 +15,47 @@ clock = pygame.time.Clock()
 x_min = 6/14 * SCREEN_WIDTH
 x_max = 8/14 * SCREEN_WIDTH
 
-objet = {"type": "random(object_types)", "x": uniform(x_min, x_max), "y": SCREEN_HEIGHT, "speed_x": uniform(-7, 7),
-         "speed_y": uniform(-17, -15), "apple": apple, "pear": pear}
 
-x_apple = uniform(x_min, x_max)
+def generate_object():
+    """"""
+    objet = {"type": "random(object_types)", "x": uniform(x_min, x_max), "y": SCREEN_HEIGHT,
+             "speed_x": uniform(-7, 7), "speed_y": uniform(-17, -15), "apple": apple, "pear": pear}
+    game_elements = [objet["x"], objet["y"], objet["speed_x"], objet["speed_y"]]
+    return game_elements
 
-x_pear = uniform(x_min, x_max)
 
-y_apple = SCREEN_HEIGHT
-y_pear = SCREEN_HEIGHT
+# objet = {"type": "random(object_types)", "x": uniform(x_min, x_max), "y": SCREEN_HEIGHT, "speed_x": uniform(-7, 7),
+#          "speed_y": uniform(-17, -15), "apple": apple, "pear": pear}
 
-speed_x_apple = uniform(-7, 7)
-speed_y_apple = uniform(-17, -15)
+fructificator1 = generate_object()
+fructificator2 = generate_object()
 
-speed_x_pear = uniform(-7, 7)
-speed_y_pear = uniform(-17, -15)
+# x_apple = uniform(x_min, x_max)
+#
+# x_pear = uniform(x_min, x_max)
+#
+# y_apple = SCREEN_HEIGHT
+# y_pear = SCREEN_HEIGHT
+#
+# speed_x_apple = uniform(-7, 7)
+# speed_y_apple = uniform(-17, -15)
+#
+# speed_x_pear = uniform(-7, 7)
+# speed_y_pear = uniform(-17, -15)
 
 t0 = pygame.time.get_ticks()  # Initial time
+
+# x_apple = objet["x"] + objet["speed_x"]
+# y_apple = objet["y"]
+
+# x_pear = objet["x"] + objet["speed_x"]
+# y_pear = objet["y"]
+
+x_apple = fructificator1[0] + fructificator1[2]
+y_apple = fructificator1[1]
+
+x_pear = fructificator2[0] + fructificator2[2]
+y_pear = fructificator1[1]
 
 running = True
 while running:
@@ -43,17 +67,17 @@ while running:
 
     t = (pygame.time.get_ticks() - t0) / 400  # Count the time during movement.
 
-    x_apple += speed_x_apple
-    y_apple += speed_y_apple * t + 5 * t**2
-
-    x_pear += speed_x_pear
-    y_pear += speed_y_pear * t + 5 * t ** 2
-
-    # x_apple = objet["x"] + objet["speed_x"]
-    # y_apple = objet["y"] + objet["speed_y"] * t + 5 * t ** 2
+    # x_apple += speed_x_apple
+    # y_apple += speed_y_apple * t + 5 * t**2
     #
-    # x_pear = objet["x"] + objet["speed_x"]
-    # y_pear = objet["y"] + objet["speed_y"] * t + 5 * t ** 2
+    # x_pear += speed_x_pear
+    # y_pear += speed_y_pear * t + 5 * t ** 2
+
+    x_apple += fructificator1[2]
+    y_apple += fructificator1[3] * t + 5 * t ** 2
+
+    x_pear += fructificator2[2]
+    y_pear += fructificator2[3] * t + 5 * t ** 2
 
     screen.fill((0, 0, 0))  # Cancel old place of image during movement.
     screen.blit(apple, (x_apple, y_apple))
